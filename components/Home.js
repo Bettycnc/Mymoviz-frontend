051ce59b7fc968b6e5a4df0e6edf7add91c9abf3
Bettycnc <betty.canac@hotmail.com>
2025-02-05 14:42:36 +0100

    first commit

--- components/Home.js
+++ components/Home.js
@@ -38,7 +38,7 @@ function Home() {
 
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-opal.vercel.app')
     .then(response => response.json())
     .then(data => {
       if(data.movies){
